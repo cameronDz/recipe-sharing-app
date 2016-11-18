d2194ede2d57bb9a7c1d889cb294234a7124b7fa
Cameron Dziurgot <cameron.dziurgot@my.ccsu.edu>
2016-11-18 10:17:43 +0000

    Fixed /db/migrate/ so Ingredients table is made.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,7 @@ class MainController < ApplicationController
   # welcome message - fetches initial number of recipes
   def welcome
       @num_recipes = Recipe.count
+      @num_ingredients = Ingredient.count
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116222357) do
+ActiveRecord::Schema.define(version: 20161118020004) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients_recipes", force: :cascade do |t|
     t.integer  "recipe_id"
