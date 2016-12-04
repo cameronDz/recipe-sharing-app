3eabf03986aff15044931f4452aabd5ce7a0e47a
nep67 <nickpipino67@gmail.com>
2016-12-04 01:00:33 +0000

    add checkbox validation

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,9 @@
 class Ingredient < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :category, presence: true, length: { maximum: 50 }
+  
   has_many :ingredients_recipes
   has_many :recipes, :through => :ingredients_recipes
+  
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,15 @@ class Recipe < ApplicationRecord
   
   validates :name,  presence: true, length: { maximum: 50 }
   validates :instructions, presence: true, length: {minimum: 5, maximum: 1000 }
+  validate :must_have_at_least_one
   
   has_many :ingredients_recipes
   has_many :ingredients, :through => :ingredients_recipes
+  
+  private
+
+  def must_have_at_least_one
+    errors.add(:base, 'You must select at least one ingredient') unless ingredient_ids.count > 0
+  end
+      
 end
